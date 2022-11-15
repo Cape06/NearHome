4f2994efb8478e5ff1e691c30a75c4ef3477f886
Cape06yt <74476735+Cape06@users.noreply.github.com>
2022-11-15 13:13:12 +0100

    Delete WebSDS directory

--- WebSDS/assets/fav.js
+++
@@ -1,7 +0,0 @@
-var list = [
-        {img: "./assets/Google.png",        link: "https://www.google.com"},
-        {img: "./assets/Drive.png",         link: "https://drive.google.com"},
-        {img: "./assets/Youtube.png",       link: "https://www.youtube.com"},
-        {img: "./assets/Ecole.jpg",         link: "https://www.ecoledirecte.com"},
-        {img: "./assets/Freepnglogos.png",  link: "https://www.freepnglogos.com"}
-]
